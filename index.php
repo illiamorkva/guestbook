588c26b64329f51c9a73517fb1ba975b4c328ac9
Illia <mor1l@yandex.ua>
2017-01-23 11:13:38 +0200

    added composer

--- index.php
+++ index.php
@@ -7,12 +7,8 @@ error_reporting(E_ALL);
 session_start();
 
 define('ROOT', dirname(__FILE__));
-//require_once(ROOT.'/components/Autoload.php');
-require_once ROOT. '/App/Components/Autoload.php';
 
-$loader = new Psr4AutoloaderClass;
-$loader->register();
-$loader->addNamespace('App', './App');
+require_once ROOT. '/vendor/autoload.php';
 
 $router = new Router();
 $router->run();
